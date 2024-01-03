4a629ff770493392eb4d6852b657f086f0ea3bdd
Mees Boeijen <mees@amees.me>
2024-01-03 15:09:32 +0100

    Adding marquee effect for testing

--- src/components/molecules/Card.tsx
+++ src/components/molecules/Card.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import classNames from "classnames";
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useState } from "react";
 import { Heading, Massive, Paragraph } from "../Typography";
 
 export type Props = {
@@ -18,15 +18,21 @@ export const Card: FC<Props> = ({
   className,
   ...rest
 }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div
       className={classNames(
         "flex-1 bg-background shadow-light rounded-[22px] p-4 pb-5 flex-col min-w-[250px] overflow-hidden cursor-pointer hover:bg-text0 hover:text-background transition-colors duration-200",
         className
       )}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
       {...rest}
     >
-      <Massive className="mb-[16px]">{title}</Massive>
+      <Massive className={classNames("mb-[16px]", isHovered && "marquee")}>
+        {title}
+      </Massive>
       <div className="flex flex-row justify-between align-bottom">
         <Paragraph variant="button">{description}</Paragraph>
       </div>
